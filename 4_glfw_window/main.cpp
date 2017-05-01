ca06f08e3f7586998c1aeb3e24c9ac9a58f31727
ramkalath <ramkalath@gmail.com>
2017-05-01 12:46:17 +0530

    first dummy commit

---
+++ 4_glfw_window/main.cpp
@@ -0,0 +1,26 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Learn OpenGL", nullptr, nullptr);
+    if(window == nullptr)
+    {
+        cout << "Failed to create a GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+	
+    // lets initialize glew
+    return 0;
+}
